17bb5dcd53000a0e10131685b30d3654d7068d14
Isaac Souza <isaacsouza@guia.dev>
2024-06-14 07:14:03 -0300

    Adjustments

--- src/application/api/rest/rest.module.ts
+++ src/application/api/rest/rest.module.ts
@@ -3,9 +3,10 @@ import { RouterModule } from '@nestjs/core';
 import { routes } from './routes';
 import { Module } from '@nestjs/common';
 import { SalesModule } from './sales/sales.module';
+import { OrderReviewModule } from './order-review/order-review.module';
 
 @Module({
-  imports: [RouterModule.register(routes), SalesModule],
+  imports: [RouterModule.register(routes), SalesModule, OrderReviewModule],
   controllers: [AppController],
 })
 export class RestModule {}
--- src/application/api/rest/routes.ts
+++ src/application/api/rest/routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@nestjs/core';
 import { SalesModule } from './sales/sales.module';
+import { OrderReviewModule } from './order-review/order-review.module';
 
 export const routes: Routes = [
 
       {
         path: '/sales',
         module: SalesModule,
+      },
+
+      {
+        path: '/order-review',
+        module: OrderReviewModule,
       },
 ];
