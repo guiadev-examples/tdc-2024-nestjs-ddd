2d6952d7c1baf7a4311018ff177643fde39b342e
Isaac Souza <isaacsouza@guia.dev>
2024-06-13 22:16:27 -0300

    Adjustment branch for microservices example

--- src/application/api/rest/order-review/order-review.module.ts
+++ src/application/api/rest/order-review/order-review.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { OrderReviewController } from './processor.controller';
+import { OrderReviewController } from './order-erview.controller';
 
 @Module({
   controllers: [OrderReviewController],
